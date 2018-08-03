bda0eee8e273f578115d51e21df5079661c46d0f
Jessica Rowland <rowlandjl@vcu.edu>
2018-08-03 18:52:46 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -10,6 +10,10 @@ class Show < ActiveRecord::Base
 
   def self.lowest_rating
     self.minimum(:rating)
+  end
+
+  def self.least_popular_show
+    self.find_by(rating: self.lowest_rating)
   end 
 
 end
